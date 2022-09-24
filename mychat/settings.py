70fa55c2f6b57336e56a1c7d2082bac7ad1362d3
sayam <iitsayamagarwal24@gmail.com>
2022-09-24 17:30:20 +0530

    edit

--- mychat/settings.py
+++ mychat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cly%(5-34f8d1pc_fi*l_p@m3^x%#a$!iq(yu=s&&ez%-_pk$3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','snapskoot.herokuapp.com']
 
 
 # Application definition
@@ -118,13 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/' 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'PROJECT_DIR', 'static‌'), ) 
-try: 
-  from local_settings import * 
-except: 
-  pass
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+STATIC_ROOT = os.path.join(BASE_DIR,'base/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
