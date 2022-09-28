a313623628645cb341f8b6a4affb3d503d5b54cb
sayam <iitsayamagarwal24@gmail.com>
2022-09-28 22:29:05 +0530

    second commit

--- mychat/settings.py
+++ mychat/settings.py
@@ -113,7 +113,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
